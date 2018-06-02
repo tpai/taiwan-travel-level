7da7e05cf65a0848e62e5bbc53bede892ff37444
Tony Pai <tony77794@gmail.com>
2018-06-03 01:22:32 +0800

    I think I might not need jQuery

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
-  const colorMap = [
+  var colorMap = [
     "#fff",
     "#27BBEE",
     "#88B14B",
@@ -7,59 +7,68 @@
     "#FF7A7A"
   ];
 
-  let cities=[{id:"新北",lv:0,},{id:"台北",lv:0,},{id:"基隆",lv:0,},{id:"桃園",lv:0,},{id:"新竹",lv:0,},{id:"苗栗",lv:0,},{id:"台中",lv:0,},{id:"彰化",lv:0,},{id:"雲林",lv:0,},{id:"嘉義",lv:0,},{id:"南投",lv:0,},{id:"台南",lv:0,},{id:"高雄",lv:0,},{id:"屏東",lv:0,},{id:"台東",lv:0,},{id:"花蓮",lv:0,},{id:"宜蘭",lv:0,},{id:"馬祖",lv:0,},{id:"金門",lv:0,},{id:"澎湖",lv:0,}];
+  var cities=[{id:"新北",lv:0,},{id:"台北",lv:0,},{id:"基隆",lv:0,},{id:"桃園",lv:0,},{id:"新竹",lv:0,},{id:"苗栗",lv:0,},{id:"台中",lv:0,},{id:"彰化",lv:0,},{id:"雲林",lv:0,},{id:"嘉義",lv:0,},{id:"南投",lv:0,},{id:"台南",lv:0,},{id:"高雄",lv:0,},{id:"屏東",lv:0,},{id:"台東",lv:0,},{id:"花蓮",lv:0,},{id:"宜蘭",lv:0,},{id:"馬祖",lv:0,},{id:"金門",lv:0,},{id:"澎湖",lv:0,}];
 
-  const contextMenu = $("#contextMenu");
-  let currentId = '';
-  let total = 0;
+  var contextMenu = document.querySelector("#contextMenu");
+  var currentId = '';
+  var total = 0;
 
   // city name text
-  $("text.city")
-    .css('cursor', 'pointer')
-    .click(bindContextMenu);
+  var cityTexts = [].map.call(document.querySelectorAll('text.city'), function (ele) { return ele; });
+  cityTexts.map(function (cityText) {
+    cityText.style.cursor = 'pointer';
+    cityText.addEventListener('click', bindContextMenu);
+  });
 
   // city area
-  cities.map(city => {
-    const dom = $("svg").find(`[id^='${city.id}']`);
-    $(dom)
-      .css('fill', 'white')
-      .css('cursor', 'pointer')
-      .click(bindContextMenu);
+  cities.map(function (city) {
+    var doms = [].map.call(document.querySelectorAll('[id^=' + city.id + ']'), function (ele) { return ele; });
+    doms.map(function (dom) {
+      dom.style.fill = '#fff';
+      dom.style.cursor = 'pointer';
+      dom.addEventListener('click', bindContextMenu);
+    });
   });
 
   // hide context menu
-  $(document).on('mouseup touchend', e => {
-    if (!contextMenu.is(e.target) && contextMenu.has(e.target).length === 0) {
-      contextMenu.hide();
+  document.addEventListener('mouseup', closeWhenClickOutside);
+  document.addEventListener('touchend', closeWhenClickOutside);
+
+  function closeWhenClickOutside (e) {
+    if (!contextMenu === e.target || !contextMenu.contains(e.target)) {
+      contextMenu.style.display = 'none';
     }
-  });
+  }
 
   // set level
-  $("div[id^='lv']").click((e) => {
-    const lv = parseInt(e.currentTarget.id.replace('lv', ''), 10);
-    cities = cities.map(city => {
-      if (city.id === currentId) {
-        return {
-          ...city,
-          lv,
-        };
-      }
-      return city;
+  var levels = [].map.call(document.querySelectorAll("div[id^='lv']"), function (ele) { return ele; });
+  levels.map(function (level) {
+    level.addEventListener('click', function (e) {
+      var lv = parseInt(e.currentTarget.id.replace('lv', ''), 10);
+      cities = cities.map(function (city) {
+        if (city.id === currentId) {
+          return {
+            ...city,
+            lv,
+          };
+        }
+        return city;
+      });
+      contextMenu.style.display = 'none';
+      changeCityColor(lv);
+      calcTotal();
     });
-    contextMenu.hide();
-    changeCityColor(lv);
-    calcTotal();
   });
 
   // save as png
-  $("#saveAs").click(() => {
+  document.querySelector('#saveAs').addEventListener('click', function () {
     var svgString = new XMLSerializer().serializeToString(document.querySelector('#map'));
     var canvas = document.createElement('canvas');
     var ctx = canvas.getContext("2d");
     canvas.width = 750;
     canvas.height = 1124;
     canvg(canvas, svgString);
-    canvas.toBlob(blob => saveAs(blob, 'taiwan.png'));
+    canvas.toBlob(function (blob) { saveAs(blob, 'taiwan.png'); });
   });
 
   function bindContextMenu (e) {
@@ -69,23 +78,24 @@
     const heightOffset = window.innerHeight - e.pageY - 165 - 30;
     const x = widthOffset > 0 ? e.pageX : e.pageX + widthOffset;
     const y = heightOffset > 0 ? e.pageY : e.pageY + heightOffset;
-    contextMenu
-      .css('left', x)
-      .css('top', y)
-      .show();
+    contextMenu.style.top = y + 'px';
+    contextMenu.style.left= x + 'px';
+    contextMenu.style.display = 'block';
     currentId = (e.target.id || e.target.textContent).replace(/\d*/g, '');
   }
 
   function calcTotal () {
     total = 0;
-    cities.map(city => {
+    cities.map(function (city) {
       total += city.lv;
     });
-    $('#total').text(total);
+    document.querySelector('#total').textContent= total;
   }
 
   function changeCityColor (lv) {
-    const dom = $("svg").find(`[id^='${currentId}']`);
-    dom.css('fill', colorMap[lv]);
+    var doms = [].map.call(document.querySelectorAll('[id^=' + currentId + ']'), function (ele) { return ele; });
+    doms.map(function (dom) {
+      dom.style.fill = colorMap[lv];
+    });
   }
 })();
